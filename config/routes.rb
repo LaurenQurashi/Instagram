3412abcf8389ca003b8ece9b8b598c598af2fa4b
Lauren <laurenqurashi@me.com>
2019-05-01 12:19:40 +0100

    Set up CSS and HTML framework for Edit User page. Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
 
   root 'home#index'
 
